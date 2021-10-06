2452f1e6770dae6ab38eaef6293a5c0dc48f3102
mkonaje <mufeed.konaje@commscope.com>
2021-10-07 00:25:22 +0530

    reformatted palindrom method

--- algorithms_and_datastructures/exercises/palindromes.py
+++ algorithms_and_datastructures/exercises/palindromes.py
@@ -4,10 +4,10 @@ Check if an input string is palindrome or not
 from functools import reduce
 
 
-def is_palindrome(string):
+def is_palindrome_1(string):
     reversed_string = reduce(lambda x, y: y + x, string)
-    return True if reversed_string == string else False
+    return reversed_string == string
 
 
 input_string = str(input())
-print(is_palindrome(input_string))
+print(is_palindrome_1(input_string))
