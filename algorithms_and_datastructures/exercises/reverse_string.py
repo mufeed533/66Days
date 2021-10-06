a7992ff3642a36d0488f50b21aa578d388ada994
mkonaje <mufeed.konaje@commscope.com>
2021-10-06 13:28:38 +0530

    added few more methods to reverse a string

--- algorithms_and_datastructures/exercises/reverse_string.py
+++ algorithms_and_datastructures/exercises/reverse_string.py
@@ -1,6 +1,7 @@
 """
 Reverse an input string
 """
+from functools import reduce
 
 
 def reverse_1(string):
@@ -15,6 +16,24 @@ def reverse_2(string):
     return string
 
 
+def reverse_3(string):
+    return string[::-1]
+
+
+def reverse_4(string):
+    output_string = ""
+    for i in string:
+        output_string = i + output_string
+    return output_string
+
+
+def reverse_5(string):
+    return reduce((lambda x, y: y + x), string)
+
+
 input_string = str(input())
 print(reverse_1(input_string))
 print(reverse_2(input_string))
+print(reverse_3(input_string))
+print(reverse_4(input_string))
+print(reverse_5(input_string))
