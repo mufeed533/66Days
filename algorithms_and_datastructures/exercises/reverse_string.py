80c877c8457a57efe860ba87df7692c55ed777b2
mkonaje <mufeed.konaje@commscope.com>
2021-10-06 12:31:35 +0530

    added function to reverse string code

--- algorithms_and_datastructures/exercises/reverse_string.py
+++ algorithms_and_datastructures/exercises/reverse_string.py
@@ -2,9 +2,13 @@
 Reverse an input string
 """
 
-input_string = str(input())
 
-output_string = ""
-for i in range(len(input_string)-1, -1, -1):
-    output_string += input_string[i]
-print(output_string)
+def reverse(string):
+    output_string = ""
+    for i in range(len(string) - 1, -1, -1):
+        output_string += string[i]
+    return output_string
+
+
+input_string = str(input())
+print(reverse(input_string))
