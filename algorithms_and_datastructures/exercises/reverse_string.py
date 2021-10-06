dbd6567c4a9c956a1972ab9206e2ac966809e398
mkonaje <mufeed.konaje@commscope.com>
2021-10-06 12:45:44 +0530

    added another method for array reversing

--- algorithms_and_datastructures/exercises/reverse_string.py
+++ algorithms_and_datastructures/exercises/reverse_string.py
@@ -3,12 +3,18 @@ Reverse an input string
 """
 
 
-def reverse(string):
+def reverse_1(string):
     output_string = ""
     for i in range(len(string) - 1, -1, -1):
         output_string += string[i]
     return output_string
 
 
+def reverse_2(string):
+    string = "".join(reversed(string))
+    return string
+
+
 input_string = str(input())
-print(reverse(input_string))
+print(reverse_1(input_string))
+print(reverse_2(input_string))
